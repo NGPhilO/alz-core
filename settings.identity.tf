7c7e90b8109b6525a6c063ecb62cf218323bf92d
phil.oakley2017@outlook.com <phil.oakley2017@outlook.com>
2024-10-16 14:37:28 +0100

    Add settings.identity.tf [skip ci]

---
+++ settings.identity.tf
@@ -0,0 +1,15 @@
+#settings.identity.tf
+
+# Configure custom identity resources settings
+locals {
+  configure_identity_resources = {
+    settings = {
+      identity = {
+        config = {
+          # Disable this policy as can conflict with Terraform
+          enable_deny_subnet_without_nsg = false
+        }
+      }
+    }
+  }
+}
